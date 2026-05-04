d2532a596fc98fe187d4fc10a05e860146709ea7
daniel-schwartz-k <daniel.schwartz@kaltura.com>
2026-05-04 12:55:18 +0300

    removed dist and readme changes

--- dist/utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.removeEmptyProps = void 0;
-const removeEmptyProps = (obj = {}, { removeEmptyString } = { removeEmptyString: false }) => {
-    const entries = Object.entries(obj).filter(([, v]) => v !== undefined && v !== null && !(removeEmptyString && v === ''));
-    return Object.fromEntries(entries);
-};
-exports.removeEmptyProps = removeEmptyProps;
